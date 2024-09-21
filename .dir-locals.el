81f79537c81225f88035def85389f335e5c1ece7
Enzo Gurgel <enzogurgel15@gmail.com>
2024-09-21 04:33:30 -0300

    update: dir.locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
-((nil . ((eval . (setq currentProject "passwordManager"))
-         (eval . (setq inferior-lisp-program "/nix/store/w432l3gnb8zj6438k4yhjh4qyd5hhmcf-sbcl-2.4.4/bin/sbcl")))))
+((nil .  (
+         (eval . (setq currentProject "passwordManager"))
+         (eval . (setq inferior-lisp-program "/nix/store/w432l3gnb8zj6438k4yhjh4qyd5hhmcf-sbcl-2.4.4/bin/sbcl"))))). 
