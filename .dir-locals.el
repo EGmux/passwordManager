503382954d8848831b9b0046bc3df008cd322fb3
Enzo Gurgel <enzogurgel15@gmail.com>
2024-09-22 19:15:15 -0300

    remove: unecessary files for this branch

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil .  (
-         (eval . (setq currentProject "passwordManager"))
-         (eval . (setq inferior-lisp-program "/nix/store/w432l3gnb8zj6438k4yhjh4qyd5hhmcf-sbcl-2.4.4/bin/sbcl"))))). 
